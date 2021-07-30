17127436cfaba66a2cfcc6ca615e66d578a475a8
oleksandr-milashechkin <nonstopjackfrost@gmail.com>
2021-07-30 23:13:26 +0300

    Update task-01

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,11 @@
-const start = document.querySelector('.item');
-console.log(start.textContent);
+const hRef = document.querySelectorAll('.item > h2');
+
+const listRef = document.querySelectorAll('.item > ul');
+
+
+const headersQuantity = hRef.length;
+
+for (let i = 0; i < headersQuantity; i+= 1) {
+    console.log(`Категория: ${hRef[i].textContent}`);
+    console.log(`Количество элементов: ${listRef[i].children.length}`);
+}
